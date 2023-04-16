1c54a8c3b8b0cd0fbd833190445a8a951cab56dd
Goff <goffxnca@gmail.com>
2023-04-16 15:38:34 +0700

    fix remote image paths

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "d31wcbk3iidrjq.cloudfront.net",
+      },
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
--- src/components/Users/UserProfile.tsx
+++ src/components/Users/UserProfile.tsx
@@ -5,16 +5,21 @@ const UserProfile = () => {
   return (
     <div className="p-10 w-[800px]">
       <div className="flex gap-x-4 items-center">
-        <img
-          src="https://d31wcbk3iidrjq.cloudfront.net/c6Gs3-B42_avatar-iZbXPK3np.jpg?h=332&w=332&q=100"
+        <Image
+          src={
+            "https://d31wcbk3iidrjq.cloudfront.net/c6Gs3-B42_avatar-iZbXPK3np.jpg?h=332&w=332&q=100"
+          }
           className="rounded-full border-4 border-accent2 w-40 h-40 p-1"
+          width={100}
+          height={100}
+          alt="cool"
         />
         <div className="text-white">
-          <h2 className="text-4xl my-2">Rosie O'Donnell</h2>
+          <h2 className="text-4xl my-2">Rosie ODonnell</h2>
           <p>
-            He's been on Friends, Seinfeld and That 70's Show. Has been in
-            cartoons like Lilo and Stitch and Invader Zim. In movies like Galaxy
-            Quest and Sky High
+            Hes been on Friends, Seinfeld and That 70 Show. Has been in cartoons
+            like Lilo and Stitch and Invader Zim. In movies like Galaxy Quest
+            and Sky High
           </p>
         </div>
         <div>{/* <ButtonLong text="Follow" /> */}</div>
