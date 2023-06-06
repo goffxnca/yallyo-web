5056bac5e84f83ee60d93f753007453b85ca0967
Goff <goffxnca@gmail.com>
2023-06-07 01:17:44 +0700

    try h-[calc(100dvh)] coz on mobile h-screen still bring scroll

--- src/pages/hey.tsx
+++ src/pages/hey.tsx
@@ -1,6 +1,6 @@
 const Hey = () => {
   return (
-    <div className="min-h-screen flex flex-col">
+    <div className="h-[calc(100dvh)] flex flex-col">
       <div className="bg-white">Header</div>
       <div className="flex-grow bg-red-200">heye</div>
     </div>
