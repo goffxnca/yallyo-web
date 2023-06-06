1fcdffc245b01e38374632c35e18b83124aee3a7
Goff <goffxnca@gmail.com>
2023-06-07 01:33:50 +0700

    cool

--- src/pages/hey.tsx
+++ src/pages/hey.tsx
@@ -2,7 +2,7 @@ const Hey = () => {
   return (
     <div className="h-screen">
       <div className="bg-white">Header</div>
-      <div className="flex-grow bg-red-200">heye</div>
+      <div className="flex-1 bg-red-200">heye</div>
     </div>
   );
 };
