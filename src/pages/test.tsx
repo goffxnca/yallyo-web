f8be5de7ec0636349a83562bb3a2a01c65ae3926
Goff <goffxnca@gmail.com>
2023-06-07 05:05:25 +0700

    cool

--- src/pages/test.tsx
+++ src/pages/test.tsx
@@ -102,9 +102,10 @@ const JoinerList = () => {
                     ? "text-white w-full md:w-1/2 md:h-[w-1/2] lg:w-1/3 max-w-[350px] max-h-[350px]"
                     : joiners <= 4
                     ? "text-white w-1/2 md:w-1/2 md:h-[w-1/3] lg:w-1/3 max-w-[400px]  max-h-[400px]"
-                    : "text-white w-1/2 h-[w-1/2] md:w-1/3 md:h-[w-1/3] max-w-[400px]  max-h-[400px]"
+                    : "text-white w-1/2 h-[w-1/2] md:w-1/3 md:h-[w-1/3] max-w-[400px]  max-h-[400px]",
+                  "p-1"
                 )}
-                style={{ backgroundColor: getRandomColor() || "red" }}
+                // style={{ backgroundColor: getRandomColor() || "red" }}
               >
                 <div className="flex items-stretch justify-center h-full w-full overflow-hidden ">
                   {/* James Doe */}
@@ -123,21 +124,20 @@ const JoinerList = () => {
                     />
 
                     <div className="bg-red-200">
-                      <div className="absolute bottom-0 bg-black text-center  w-full bg-opacity-50 p-1 md:p-3 ">
-                        <div className="flex justify-center items-center">
+                      <div className="absolute bottom-0 bg-black text-center  bg-opacity-70 p-1 md:p-3 ">
+                        <div className="flex items-center">
                           {/* Mic */}
                           <div className="relative ml-1">
                             <MicrophoneIcon className="w-4 h-4 md:w-5 md:h-5 text-red-500" />
-                            <div className="absolute top-0 left-0 right-0 bottom-0">
+                            {/* <div className="absolute top-0 left-0 right-0 bottom-0">
                               <div className="flex justify-center items-center w-full h-full">
                                 <div
                                   className={`border border-red-500 w-8 rotate-45`}
                                 ></div>
                               </div>
-                            </div>
+                            </div> */}
                           </div>
-                          <div className="text-xs md:text-lg overflow-ellipsis">
-                            {" "}
+                          <div className="text-xs md:text-lg font-bold overflow-ellipsis">
                             {name}
                           </div>
                         </div>
