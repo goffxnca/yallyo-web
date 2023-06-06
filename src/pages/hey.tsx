6a65f7c329b6c7c63cc23e435827b4207e5949b0
Goff <goffxnca@gmail.com>
2023-06-07 02:06:01 +0700

    try -webkit-fill-available

--- src/pages/hey.tsx
+++ src/pages/hey.tsx
@@ -1,27 +1,10 @@
 import { useEffect } from "react";
 
 const Hey = () => {
-  useEffect(() => {
-    function setRootHeight() {
-      const root = document.documentElement;
-      const vh = window.innerHeight * 0.01;
-      root.style.setProperty("--vh", `${vh}px`);
-    }
-
-    window.addEventListener("resize", setRootHeight);
-    setRootHeight();
-
-    return () => {
-      window.removeEventListener("resize", setRootHeight);
-    };
-  }, []);
-
   return (
-    <div className="flex flex-col h-screen">
-      <div className="bg-white">Header</div>
-      <div className="bg-red-200 flex-grow">
-        Should be full without scrolling
-      </div>
+    <div id="container" className="flex flex-col h-screen">
+      <div className="bg-white">Heade22r</div>
+      <div className="bg-red-200 flex-grow">Should be</div>
     </div>
   );
 };
