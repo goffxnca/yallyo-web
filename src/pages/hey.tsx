f3c1cab2ba79b7969e49c01be4a0a8cfcaeb9817
Goff <goffxnca@gmail.com>
2023-06-07 01:32:27 +0700

    ccc

--- src/pages/hey.tsx
+++ src/pages/hey.tsx
@@ -1,6 +1,6 @@
 const Hey = () => {
   return (
-    <div className="h-[calc(100dvh)] flex flex-col">
+    <div className="h-screen">
       <div className="bg-white">Header</div>
       <div className="flex-grow bg-red-200">heye</div>
     </div>
