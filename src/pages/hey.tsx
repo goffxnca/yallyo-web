5946fa87b7e42d748364aefaf77bd71b65e01f22
Goff <goffxnca@gmail.com>
2023-06-07 03:08:29 +0700

    try again

--- src/pages/hey.tsx
+++ src/pages/hey.tsx
@@ -2,9 +2,11 @@ import { useEffect } from "react";
 
 const Hey = () => {
   return (
-    <div id="container" className="flex flex-col h-screen">
+    <div id="container" className="flex flex-col h-screen relative">
+      <div className="text-white">Top</div>
       <div className="bg-white">Heade22r</div>
       <div className="bg-red-200 flex-grow">Should be</div>
+      <div className="text-white">Bottom</div>
     </div>
   );
 };
