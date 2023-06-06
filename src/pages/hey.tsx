ff606a0312fcf20484a5fdff1b41833100e4bc71
Goff <goffxnca@gmail.com>
2023-06-07 01:51:44 +0700

    cool2

--- src/pages/hey.tsx
+++ src/pages/hey.tsx
@@ -1,8 +1,27 @@
+import { useEffect } from "react";
+
 const Hey = () => {
+  useEffect(() => {
+    function setRootHeight() {
+      const root = document.documentElement;
+      const vh = window.innerHeight * 0.01;
+      root.style.setProperty("--vh", `${vh}px`);
+    }
+
+    window.addEventListener("resize", setRootHeight);
+    setRootHeight();
+
+    return () => {
+      window.removeEventListener("resize", setRootHeight);
+    };
+  }, []);
+
   return (
-    <div className="">
-      <div className="bg-white">Heade22r</div>
-      <div className=" bg-red-200">heye22</div>
+    <div className="flex flex-col h-screen">
+      <div className="bg-white">Header</div>
+      <div className="bg-red-200 flex-grow">
+        Should be full without scrolling
+      </div>
     </div>
   );
 };
