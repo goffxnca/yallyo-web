7ca1da640c513be1f4d3ccfa31f07b53abe7271d
Goff <goffxnca@gmail.com>
2023-06-07 02:11:13 +0700

    try -webkit-fill-available22

--- src/pages/hey.tsx
+++ src/pages/hey.tsx
@@ -2,7 +2,7 @@ import { useEffect } from "react";
 
 const Hey = () => {
   return (
-    <div id="container" className="flex flex-col h-screen">
+    <div id="container" className="flex flex-col h-screen overflow-hidden">
       <div className="bg-white">Heade22r</div>
       <div className="bg-red-200 flex-grow">Should be</div>
     </div>
