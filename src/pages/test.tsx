3fcccda3524c33285bcc55148e10e5787b6ef81c
Goff <goffxnca@gmail.com>
2023-06-08 00:26:29 +0700

    yo

--- src/pages/test.tsx
+++ src/pages/test.tsx
@@ -39,7 +39,7 @@ const JoinerList = () => {
   };
 
   useEffect(() => {
-    if (typeof window !== "undefined") {
+    if (typeof window !== "undefined" && window.navigator) {
       setReady(true);
       renderLocalVideoStream();
     }
@@ -99,7 +99,7 @@ const JoinerList = () => {
                 // className="text-white w-full md:h-1/2  md:w-1/2 md:max-w-1/2 max-w-[500px] max-h-[500px]"
                 className={joinClasses(
                   joiners <= 2
-                    ? "text-white w-full md:w-1/2 md:h-[w-1/2] lg:w-1/3"
+                    ? "text-white w-full md:w-1/2 md:h-[w-1/2] lg:w-1/2"
                     : joiners <= 4
                     ? "text-white w-1/2 md:w-1/2 md:h-[w-1/3] lg:w-1/3"
                     : "text-white w-1/2 h-[w-1/2] md:w-1/3 md:h-[w-1/3]",
