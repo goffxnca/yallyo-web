22a34bf581a9ed6cf875cdc0a37ef0a02cc8dd3a
Goff <goffxnca@gmail.com>
2023-07-06 15:01:00 +0700

    add google anaylytics

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,9 +1,24 @@
 import { Html, Head, Main, NextScript } from "next/document";
+import Script from "next/script";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <Script
+          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_FIREBASE_measurementId}`}
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', '${process.env.NEXT_PUBLIC_FIREBASE_measurementId}');
+        `}
+        </Script>
+      </Head>
       <body>
         <Main />
         <NextScript />
