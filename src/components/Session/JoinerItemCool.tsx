698aedc06ee607058551209f80a9647b0ea9f00d
Goff <goffxnca@gmail.com>
2023-07-15 16:10:24 +0700

    test

--- src/components/Session/JoinerItemCool.tsx
+++ src/components/Session/JoinerItemCool.tsx
@@ -63,7 +63,7 @@ const JoinerItemCool = ({
       >
         {/* James Doe */}
         <div className="relative w-full overflow-hidden">
-          {/* {controls && !controls.camOn && (
+          {controls && !controls.camOn && (
             <div className="w-full h-full ">
               <div className="text-white flex justify-center items-center h-full">
                 <div className="text-white flex justify-center">
@@ -84,7 +84,7 @@ const JoinerItemCool = ({
                 isMe={isMe}
               />
             </div>
-          )} */}
+          )}
 
           <div className="relative h-full w-full">
             <div className="rounded-lg overflow-hidden z-20">
