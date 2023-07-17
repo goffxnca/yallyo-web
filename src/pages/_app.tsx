049d4e238dd16ed4a72cd198ac4b085a749ec952
Goff <goffxnca@gmail.com>
2023-07-17 15:57:04 +0700

    add logrocket

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,8 +5,17 @@ import type { AppProps } from "next/app";
 import { Provider } from "react-redux";
 import Auth from "@/components/Layouts/Auth";
 import store from "@/store/store";
+import LogRocket from "logrocket";
+import { useEffect } from "react";
 
 export default function App({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    if (process.env.NODE_ENV === "production") {
+      console.log("Register log rocket");
+      LogRocket.init("l36g1y/yallyo");
+    }
+  }, []);
+
   return (
     <Provider store={store}>
       <Auth />
