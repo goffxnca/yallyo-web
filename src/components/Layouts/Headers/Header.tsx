bcf2d2f1811829d9ebab510221d80feec4ecd120
Goff <goffxnca@gmail.com>
2023-07-18 11:46:08 +0700

    change title

--- src/components/Layouts/Headers/Header.tsx
+++ src/components/Layouts/Headers/Header.tsx
@@ -28,7 +28,7 @@ const Header = () => {
             Yallyo.com
           </h1>
           <h2 className="text-gray-400 text-xs md:text-sm">
-            Talk to Strangers worldwide
+            Talk to Strangers Worldwide!
           </h2>
         </div>
       )}
