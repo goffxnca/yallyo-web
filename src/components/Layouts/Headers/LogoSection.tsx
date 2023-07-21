01818e6d511ec67362cf8835392043591f4735e2
Goff <goffxnca@gmail.com>
2023-07-21 21:06:45 +0700

    bring back logo

--- src/components/Layouts/Headers/LogoSection.tsx
+++ src/components/Layouts/Headers/LogoSection.tsx
@@ -5,9 +5,9 @@ const LogoSection = () => {
   return (
     <Link href="/">
       <div className="text-accent1 font-bold text-xl hover:scale-105">
-        {/* <div className="text-accent1 text-3xl">
+        <div className="text-accent1 text-3xl">
           <img src={"/images/yallyo9.png"} alt="Yallyo Logo" className="w-28" />
-        </div> */}
+        </div>
 
         {/* <div className="text-gray-100  text-[10px] -rotate-6 -mt-3">
           Language Community
