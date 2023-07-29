f91b147d76542f70e7172b519dae3f4ff325fe67
Goff <goffxnca@gmail.com>
2023-07-29 12:10:45 +0700

    update isSameDate function to localize before compare

--- src/utils/date-utils.ts
+++ src/utils/date-utils.ts
@@ -11,9 +11,9 @@ function isSameDate(iosDate1: string, isoDate2: string) {
   const date2 = new Date(isoDate2);
 
   return (
-    date1.getUTCFullYear() === date2.getUTCFullYear() &&
-    date1.getUTCMonth() === date2.getUTCMonth() &&
-    date1.getUTCDate() === date2.getUTCDate()
+    date1.getFullYear() === date2.getFullYear() &&
+    date1.getMonth() === date2.getMonth() &&
+    date1.getDate() === date2.getDate()
   );
 }
 
