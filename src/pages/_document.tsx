996f463d29756f6e7aa9ba4695f1abf7683c5c9d
Goff <goffxnca@gmail.com>
2023-08-03 18:39:31 +0700

    add facebook pixels

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -18,6 +18,28 @@ export default function Document() {
           gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID}');
         `}
         </Script>
+
+        <Script id="facebook-pixels" strategy="afterInteractive">
+          {`
+            <!-- Meta Pixel Code -->
+            <script>
+              !function(f,b,e,v,n,t,s)
+              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+              n.queue=[];t=b.createElement(e);t.async=!0;
+              t.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)}(window, document,'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+              fbq('init', ${process.env.NEXT_PUBLIC_FACEBOOK_PIXELS_ID});
+              fbq('track', 'PageView');
+            </script>
+            <noscript><img height="1" width="1" style="display:none"
+              src="https://www.facebook.com/tr?id=${process.env.NEXT_PUBLIC_FACEBOOK_PIXELS_ID}&ev=PageView&noscript=1"
+            /></noscript>
+            <!-- End Meta Pixel Code -->
+        `}
+        </Script>
       </Head>
       <body>
         <Main />
