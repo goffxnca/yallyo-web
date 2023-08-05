ebdea31b6b807ee5c832862f45a4bd24563338b4
Goff <goffxnca@gmail.com>
2023-08-05 14:08:46 +0700

    fix build fail

--- src/pages/blog/enhancing-english-fluency-through-yallyo-s-voice.tsx
+++ src/pages/blog/enhancing-english-fluency-through-yallyo-s-voice.tsx
@@ -11,7 +11,11 @@ const Blog1Page = () => {
         title="Finding Your Voice: Enhancing English Fluency Through Yallyo's Voice Chat"
         desc="Discover how Yallyo, a revolutionary voice-chat platform, connects strangers worldwide for English language practice. Engage in global conversations, break the ice, and form meaningful connections across cultures."
       />
-      <BlogHeader title="Finding Your Voice: Enhancing English Fluency Through Yallyo's Voice Chat" />
+      <BlogHeader
+        title="Finding Your Voice: Enhancing English Fluency Through Yallyo's Voice Chat"
+        heroImageUrl=""
+        heroImageAlt=""
+      />
       <BlogSection
         title="Introduction"
         paragraph1="In a rapidly globalizing world, effective communication has become more important than ever. English, as the lingua franca of the modern era, plays a significant role in connecting people from different cultures and backgrounds. However, learning a new language can be a daunting task, and practice opportunities with native speakers are not always readily available. This is where Yallyo comes in – a revolutionary voice-chat platform that brings strangers together from across the globe to practice English speaking in a friendly and supportive environment. In this blog post, we will explore how Yallyo is breaking the ice and fostering meaningful connections among language learners worldwide."
