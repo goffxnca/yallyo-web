534e7f4777b4a20d13605fcfbcb40882dbcf4904
Goff <goffxnca@gmail.com>
2023-08-15 10:26:34 +0700

    turn on logrocket

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,14 +6,16 @@ import { Provider } from "react-redux";
 import Auth from "@/components/Layouts/Auth";
 import store from "@/store/store";
 import Alert from "@/components/Layouts/Alert";
+import { useEffect } from "react";
+import LogRocket from "logrocket";
 
 export default function App({ Component, pageProps }: AppProps) {
-  // useEffect(() => {
-  //   if (process.env.NODE_ENV === "production") {
-  //     console.log("Register log rocket");
-  //     LogRocket.init("l36g1y/yallyo");
-  //   }
-  // }, []);
+  useEffect(() => {
+    if (process.env.NODE_ENV === "production") {
+      console.log("Register log rocket");
+      LogRocket.init("l36g1y/yallyo");
+    }
+  }, []);
 
   const noLayout = (Component as any)["noLayout"];
 
