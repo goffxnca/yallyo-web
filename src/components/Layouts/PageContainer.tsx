b6fd2a7cc4e0e88554d89442e189b5d51707d5ba
Goff <goffxnca@gmail.com>
2023-08-16 20:30:22 +0700

    yeah

--- src/components/Layouts/PageContainer.tsx
+++ src/components/Layouts/PageContainer.tsx
@@ -5,7 +5,7 @@ interface Props {
 }
 
 const PageContainer = ({ children }: Props) => {
-  return <div className="px-4 py-20 grid bg-primary">{children}</div>;
+  return <div className="px-2 md:px-4 py-20 grid bg-primary">{children}</div>;
 };
 
 export default PageContainer;
