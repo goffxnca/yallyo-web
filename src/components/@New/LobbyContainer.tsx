8e792b510e76ff38048de2d33d7fa2bb8d1bd9f2
hyeonho64 <hyeonhokang10@kakao.com>
2023-10-08 15:19:29 +0900

    Fix build errors.

--- src/components/@New/LobbyContainer.tsx
+++ src/components/@New/LobbyContainer.tsx
@@ -1,34 +1,40 @@
-import { toggleLobbyContainer } from "@/store/layoutSlice";
-import { fetchLobbyChatAsync } from "@/store/lobbyChatSlice";
-import { AppDispatch, RootState } from "@/store/store";
-import { createNArray } from "@/utils/array-utils";
-import { ChevronLeftIcon, ChevronRightIcon } from "@heroicons/react/24/outline";
+import PageContainer from "@/components/Layouts/PageContainer";
+import {
+  createLobbyChatAsync,
+  fetchLobbyChatAsync,
+} from "@/store/lobbyChatSlice";
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import LobbyChatList from "../Lobby/LobbyChatList";
-import { useMediaQuery } from "usehooks-ts";
+import { AppDispatch, RootState } from "@/store/store";
+import LobbyChatList from "@/components/Lobby/LobbyChatList";
+import { subscribeLobbyChatUpdates } from "@/libs/ws-subscriptions";
 
-const LobbyContainer = () => {
-  const isDesktop = useMediaQuery("(min-width: 1024px)");
-
-  const { lobbyContainerExpanded } = useSelector(
-    (state: RootState) => state.layout
-  );
-
+const LobbyPage = () => {
+  const dispatch: AppDispatch = useDispatch();
   const {
     lobbyChats,
     status: lobbyChatStatus,
     canLoadMore: canLoadLobbyChatMore,
     lastFetchedItemId,
+    lastAddedItemId,
   } = useSelector((state: RootState) => state.lobbyChat);
 
-  const dispatch: AppDispatch = useDispatch();
+  const loadMoreLobbyChatMessages = () => {
+    dispatch(
+      fetchLobbyChatAsync({
+        psize: 5,
+        cursor: lastFetchedItemId,
+      })
+      );
+  };
 
+  // Subscribe to room update
   useEffect(() => {
-    if (isDesktop) {
-      dispatch(toggleLobbyContainer());
-    }
-  }, [isDesktop, dispatch]);
+    const lobbyChatSocket = subscribeLobbyChatUpdates(dispatch);
+    return () => {
+      lobbyChatSocket.disconnect();
+    };
+    }, [dispatch]);
 
   useEffect(() => {
     if (dispatch) {
@@ -37,44 +43,12 @@ const LobbyContainer = () => {
           psize: 20,
           cursor: "",
         })
-      );
+        );
     }
   }, [dispatch]);
 
   return (
-    <div
-      className={`absolute md:relative left-0 z-30 ${
-        lobbyContainerExpanded ? "w-11/12 md:w-3/12" : "w-0 md:w-[50px]"
-      } overflow-scroll bg-green-200`}
-    >
-      <div className="text-right">
-        {lobbyContainerExpanded ? (
-          <button
-            onClick={() => {
-              dispatch(toggleLobbyContainer());
-            }}
-            className=""
-          >
-            <ChevronLeftIcon className="w-5 h-5" />
-          </button>
-        ) : (
-          <button
-            onClick={() => {
-              dispatch(toggleLobbyContainer());
-            }}
-            className=""
-          >
-            <ChevronRightIcon className="w-5 h-5" />
-          </button>
-        )}
-      </div>
-
-      {/* {createNArray(100).map((value, index) => (
-        <div key={index} className="">
-          Lobby Message {index}
-        </div>
-      ))} */}
-
+    <PageContainer>
       <LobbyChatList
         lobbyChats={lobbyChats}
         isLoading={lobbyChatStatus === "loading"}
@@ -85,8 +59,8 @@ const LobbyContainer = () => {
       }}
         lastAddedItemId={lastAddedItemId}
       />
-    </div>
-  );
+    </PageContainer>
+    );
 };
 
 export default LobbyContainer;
